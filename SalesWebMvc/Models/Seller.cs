0a41169f32066ce659bbcdde579e6752f43fd732
Matheus Gabriell <matheuslinx0@gmail.com>
2025-02-01 17:16:26 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -6,18 +6,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60,MinimumLength = 3)]
         public string Name { get; set; }
 
-
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "E-mail")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
